3a8a755cc72bd6cd8b557978490e6e003492947c
Fahmief <57023807+fahmief@users.noreply.github.com>
2024-11-25 22:18:37 +0700

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-const swiper = new Swiper(".slider-wrapper", {
+const swiper1 = new Swiper(".slider-wrapper", {
   // Optional parameters
   //   direction: "vertical",
   loop: true,
@@ -28,6 +28,45 @@ const swiper = new Swiper(".slider-wrapper", {
     },
     1024: {
       slidesPerView: 2,
+    },
+  },
+
+  // And if we need scrollbar
+  //   scrollbar: {
+  //     el: ".swiper-scrollbar",
+  //   },
+});
+
+const swiper2 = new Swiper(".slider-wrapper2", {
+  // Optional parameters
+  //   direction: "vertical",
+  loop: true,
+  gabCursor: true,
+  spaceBetween: 30,
+
+  // If we need pagination
+  pagination: {
+    el: ".slider-wrapper2 .swiper-pagination",
+    clickable: true,
+    dynamicBullets: true,
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".slider-wrapper2 .swiper-button-next",
+    prevEl: ".slider-wrapper2 .swiper-button-prev",
+  },
+
+  //   responsive positioning
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+    1024: {
+      slidesPerView: 3,
     },
   },
 
