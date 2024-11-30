b7c8e5bacff39d69802a0619ca09ea399bd8cd24
Fahmief <57023807+fahmief@users.noreply.github.com>
2024-11-30 21:34:55 +0700

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -75,3 +75,81 @@ const swiper2 = new Swiper(".slider-wrapper2", {
   //     el: ".swiper-scrollbar",
   //   },
 });
+
+const swiper3 = new Swiper(".slider-wrapper3", {
+  // Optional parameters
+  //   direction: "vertical",
+  loop: true,
+  gabCursor: true,
+  spaceBetween: 30,
+
+  // If we need pagination
+  pagination: {
+    el: ".slider-wrapper3 .swiper-pagination",
+    clickable: true,
+    dynamicBullets: true,
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".slider-wrapper3 .swiper-button-next",
+    prevEl: ".slider-wrapper3 .swiper-button-prev",
+  },
+
+  //   responsive positioning
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+    1024: {
+      slidesPerView: 3,
+    },
+  },
+
+  // And if we need scrollbar
+  //   scrollbar: {
+  //     el: ".swiper-scrollbar",
+  //   },
+});
+
+const swiper4 = new Swiper(".slider-wrapper4", {
+  // Optional parameters
+  //   direction: "vertical",
+  loop: true,
+  gabCursor: true,
+  spaceBetween: 30,
+
+  // If we need pagination
+  pagination: {
+    el: ".slider-wrapper4 .swiper-pagination",
+    clickable: true,
+    dynamicBullets: true,
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".slider-wrapper4 .swiper-button-next",
+    prevEl: ".slider-wrapper4 .swiper-button-prev",
+  },
+
+  //   responsive positioning
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+    1024: {
+      slidesPerView: 3,
+    },
+  },
+
+  // And if we need scrollbar
+  //   scrollbar: {
+  //     el: ".swiper-scrollbar",
+  //   },
+});
